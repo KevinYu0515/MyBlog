389b6707f1b9e6256f86e1ec636e4bfe91d565a6
Kevin051596 <yellow0515car@gmail.com>
2023-05-30 21:42:13 +0800

    add: TinaCMS

---
+++ tina/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "e3d51461-6e8f-4f58-885d-ed9f217f36ba", // Get this from tina.io
+  token: "38fff050e8632637f52dc62d9db74dd32294a82e", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/posts",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
